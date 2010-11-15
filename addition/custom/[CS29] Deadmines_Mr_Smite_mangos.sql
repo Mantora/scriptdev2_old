8e664e1604111ec97ec4e6069299a87970505108
Berni <Berni@.(none)>
2010-11-15 23:04:48 +0100

    [CS59] some Corrections (SQL and Scripts)

--- addition/custom/[CS29] Deadmines_Mr_Smite_mangos.sql
+++ addition/custom/[CS29] Deadmines_Mr_Smite_mangos.sql
@@ -1,3 +1,3 @@
--- Scriptname
+-- script for Mr. Smite in instance Deadmines
 DELETE FROM `creature_ai_scripts` WHERE `creature_id`='646';
 UPDATE `creature_template` SET `AIName`='',`ScriptName`='boss_mrsmite' WHERE (`entry`='646');
